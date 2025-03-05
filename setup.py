5ede15e21d8ec06764f4d812843599be8417d170
heer <1767407822@qq.com>
2025-03-05 22:02:17 +0800

    🐛 修复JS文件没有被包含的问题

--- setup.py
+++ setup.py
@@ -11,16 +11,19 @@ setup(
         'PyExecJS',  # 用于执行JavaScript代码
         'pycryptodome'
     ],
+    package_data={
+        'just_kit': ['js/*'],
+    },
     author="Harmog",
     author_email="harmog@foxmail.com",
     description="江苏科技大学信息门户工具包",
-    # long_description=open('README.md').read(),
-    # long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/heerheer/just-kit",
     # classifiers=[
     #     "Programming Language :: Python :: 3",
     #     "License :: OSI Approved :: MIT License",
     #     "Operating System :: OS Independent",
     # ],
-    # python_requires='>=3.6',
+    # python_requires='>=3.11',
 )
