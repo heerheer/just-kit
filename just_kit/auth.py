9e9ee0fd651632bc872380bf05dc5649149b93ec
heer <1767407822@qq.com>
2025-06-01 17:03:07 +0800

    更改RSA密码的加密方式，丢弃js拥抱py

--- just_kit/auth.py
+++ just_kit/auth.py
@@ -11,12 +11,13 @@ class Authenticator:
     '''
     用于登录信息门户的类
     '''
-    def __init__(self, service: str = "http://my.just.edu.cn/", debug=False,auto_login=True):
+    def __init__(self, service: str = "http://my.just.edu.cn/", debug=False,auto_login=True,vpn:bool=False):
         '''
         初始化函数
         :param service: 登录的服务,推荐使用默认值 http://my.just.edu.cn/
         :param debug: 是否开启调试模式
         :param auto_login: 是否自动读取保存的cookies以快速登录
+        :param vpn: 是否使用vpn,若使用则会service使用ids2.just.edu.cn
         '''
         self.logger = logging.getLogger(__name__)
         if debug:
@@ -27,7 +28,7 @@ class Authenticator:
         else:
             logging.basicConfig(level=logging.INFO)
         # 用于储存登入后的一些数据
-        self.service = service
+        self.service = 'https://ids.v.just.edu.cn/' if vpn else service
         self.login_data = {}
         self.session = requests.Session()
         self.cookie_file = ".cookies_" + \
@@ -73,39 +74,16 @@ class Authenticator:
         d = self.session.cookies
         return d["JSESSIONID"] if 'JSESSIONID' in d else None
 
-    def encrypt_with_js(self, password) -> Optional[str]:
-
+    @staticmethod
+    def encrypt_with_js(password) -> str:
+        import utils.rsa as rsa
         """
-        使用 js 脚本加密数据
+        使用 与js脚本一直的方式进行加密数据
         :param password: 要加密的密码
         :return: 加密后的数据
         """
-        try:
-            import execjs
-            import os
-            # 构建 js 文件的绝对路径
-            js_path = os.path.join(
-                os.path.dirname(__file__), "js", "security.js")
-            # 读取加密脚本
-            with open(js_path, "r", encoding="utf-8") as f:
-                js_code = f.read()
-            code = '''
-            // 获取命令行参数
-            function encrypt(data) {
-                RSAUtils.setMaxDigits(131);
-                var key = RSAUtils.getKeyPair("010001", '', "008aed7e057fe8f14c73550b0e6467b023616ddc8fa91846d2613cdb7f7621e3cada4cd5d812d627af6b87727ade4e26d26208b7326815941492b2204c3167ab2d53df1e3a2c9153bdb7c8c2e968df97a5e7e01cc410f92c4c2c2fba529b3ee988ebc1fca99ff5119e036d732c368acf8beba01aa2fdafa45b21e4de4928d0d403");
-                return RSAUtils.encryptedString(key, data);
-            }
-            '''
-            js_code += code
-            # 创建一个JavaScript上下文
-            ctx = execjs.compile(js_code)
-            result = ctx.call("encrypt", password)
-            return result
-        except Exception as e:
-            self.logger.error(f"Error executing JavaScript: {e}")
-            return None
-    
+        return rsa.encrypt(password)
+
     def login(self, account: str, password: str):
 
         if self.check():
@@ -119,33 +97,12 @@ class Authenticator:
         with self.session as session:
 
             self.headers['HOST'] = get_host_from_url(self.service)
-            # 直接访问service并得到跳转地址
+            # 直接访问service进行自动跳转?
             res = session.get(
                 self.service,
-                headers=self.headers,
-                allow_redirects=False,
+                allow_redirects=True,
             )
-            print( res.cookies.get_dict())
-            # 该服务登入地址
-            target = res.headers["Location"]
-            # debug
-            self.logger.debug(f"---第一次访问---")
-            self.logger.debug(f"{res.status_code}->{target}")
-            self.logger.debug(res.headers)
-            self.logger.debug(session.cookies.get_dict())
-            # 在跳转时要重置HOST和Origin防止404
-            self.headers['HOST'] = get_host_from_url(target)
-            self.headers["Origin"] = get_origin(target)
-
-            res = session.get(
-                abs_url(self.service,target),
-                headers=self.headers,
-                allow_redirects=False,
-            )
-            # debug
-            self.logger.debug(f"{res.status_code}->{target}")
-            self.logger.debug(res.headers)
-            self.logger.debug(session.cookies.get_dict())
+            target = res.url
             # find execution
             soup = BeautifulSoup(res.text, "html.parser")
             execution_input = soup.find("input", {"name": "execution"})
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="just_kit",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=[
         'requests',  # 用于发送HTTP请求
