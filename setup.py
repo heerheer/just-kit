e1e5feb794a34cccd7f2389bde3880b0dd72ad23
heer <1767407822@qq.com>
2025-06-01 21:36:26 +0800

    ✨ v0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="just_kit",
-    version="0.1.3",
+    version="0.2.0",
     packages=find_packages(),
     install_requires=[
         'requests',  # 用于发送HTTP请求
